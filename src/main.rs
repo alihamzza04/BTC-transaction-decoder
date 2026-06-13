326701ab72b052d6f255bef01e7ee4a6e3bab192
alihamzza04 <ali.javed.hamza@gmail.com>
2026-06-13 10:01:10 +0500

    Module 3 Completed BTCdemy

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,34 @@
+use serde::Serialize;
 use std::io::Read;
+
+#[derive(Debug, Serialize)]
+struct Transaction {
+    version: u32,
+    inputs: Vec<Input>,
+    outputs: Vec<Output>,
+}
+
+#[derive(Debug, Serialize)]
+struct Input {
+    txid: String,
+    output_index: u32,
+    script_sig: String,
+    sequence: u32,
+}
+
+struct Amount(u64);
+
+impl Amount {
+    pub fn to_btc(&self) -> f64 {
+        self.0 as f64 / 100_000_000.0
+    }
+}
+
+#[derive(Debug, Serialize)]
+struct Output {
+    amount: f64,
+    script_pubkey: String,
+}
 
 fn read_compact_size(transaction_bytes: &mut &[u8]) -> u64 {
     let mut compact_size = [0_u8; 1];
@@ -24,11 +54,32 @@ fn read_compact_size(transaction_bytes: &mut &[u8]) -> u64 {
     }
 }
 
-fn read_version(transaction_bytes: &mut &[u8]) -> u32 {
+fn read_amount(transaction_bytes: &mut &[u8]) -> Amount {
+    let mut buffer = [0; 8];
+    transaction_bytes.read_exact(&mut buffer).unwrap();
+
+    Amount(u64::from_le_bytes(buffer))
+}
+
+fn read_u32(transaction_bytes: &mut &[u8]) -> u32 {
     let mut buffer = [0; 4];
     transaction_bytes.read_exact(&mut buffer).unwrap();
 
     u32::from_be_bytes(buffer)
+}
+
+fn read_txid(transaction_bytes: &mut &[u8]) -> String {
+    let mut buffer = [0; 32];
+    transaction_bytes.read_exact(&mut buffer).unwrap();
+    buffer.reverse();
+    hex::encode(buffer)
+}
+
+fn read_script(transaction_bytes: &mut &[u8]) -> String {
+    let script_size = read_compact_size(transaction_bytes) as usize;
+    let mut buffer = vec![0_u8; script_size];
+    transaction_bytes.read_exact(&mut buffer).unwrap();
+    hex::encode(buffer)
 }
 
 fn main() {
@@ -36,18 +87,54 @@ fn main() {
     let transaction_bytes = hex::decode(transaction_hex).unwrap();
     let mut bytes_slice = transaction_bytes.as_slice();
 
-    let version = read_version(&mut bytes_slice);
-    let input_length = read_compact_size(&mut bytes_slice);
-    println!("Version: {}", version);
-    println!("Input length: {}", input_length);
+    let version = read_u32(&mut bytes_slice);
+    let input_count = read_compact_size(&mut bytes_slice);
+    let mut inputs = vec![];
+
+    for _ in 0..input_count {
+        let txid = read_txid(&mut bytes_slice);
+        let output_index = read_u32(&mut bytes_slice);
+        let script_sig = read_script(&mut bytes_slice);
+        let sequence = read_u32(&mut bytes_slice);
+
+        inputs.push(Input {
+            txid,
+            output_index,
+            script_sig,
+            sequence,
+        });
+    }
+
+    let output_count = read_compact_size(&mut bytes_slice);
+    let mut outputs = vec![];
+
+    for _ in 0..output_count {
+        let amount = read_amount(&mut bytes_slice).to_btc();
+        let script_pubkey = read_script(&mut bytes_slice);
+
+        outputs.push(Output {
+            amount,
+            script_pubkey,
+        });
+    }
+
+    let transaction = Transaction {
+        version,
+        inputs,
+        outputs,
+    };
+    println!(
+        "Transaction: {}",
+        serde_json::to_string_pretty(&transaction).unwrap()
+    );
 }
 
 #[cfg(test)]
-mod test{
+mod test {
     use super::read_compact_size;
 
     #[test]
-    fn test_read_compact_size(){
+    fn test_read_compact_size() {
         let mut bytes = [1_u8].as_slice();
         let count = read_compact_size(&mut bytes);
         assert_eq!(count, 1_u64);
